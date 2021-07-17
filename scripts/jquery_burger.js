d780956aa0800f5a4633ba089d9c1a6fd87720cd
Pablo <pavlo.laptiev@gmail.com>
2021-07-17 20:54:57 +0300

    first commit

--- scripts/jquery_burger.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.header__burger').click(function(event){
-        $('.header__burger').toggleClass('header__burger_active');
-        $('.header__menu').toggleClass('header__menu_active');
-        $('body').toggleClass('lock');
-    })
-});
