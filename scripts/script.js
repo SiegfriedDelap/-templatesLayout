4551b0645fd689ca99d08b3e79d4d9d97ceb055b
Pablo <pavlo.laptiev@gmail.com>
2021-07-17 23:13:01 +0300

    js script done

--- scripts/script.js
+++ scripts/script.js
@@ -7,10 +7,19 @@ window.addEventListener('DOMContentLoaded', ()=>{
     const right = document.getElementById('oceanRight');
     const text = document.querySelector('.ocean__title');
     const btn = document.querySelector('.ocean__btn');
+    const header = document.querySelector('.header');
 
     window.addEventListener('scroll', ()=>{
         let value = window.scrollY;
-        parts.style.left = value + 'px';
+        parts.style.left = value * 0.25 + 'px';
+        bg.style.top = value * 0.5 + 'px';
+        left.style.top = value * 0 + 'px';
+        right.style.top = value * 0 + 'px';
+        text.style.marginRight = value * 3 + 'px';
+        text.style.marginTop = value * 0.25 + 'px';
+        btn.style.marginTop = value * 0.5 + 'px';
+        header.style.top = value * 0.6 + 'px';
+
     });
 
 
